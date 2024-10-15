7ffe64e73a8eef89d0c54acda6e7791d36001214
ТехноРай <sofialalacha@007>
2024-10-15 10:22:43 +0300

    feat: initial commit

---
+++ lalak_sample_project/hello_app/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+
+def hello(request, name):
+    return HttpResponse("Name: {}".format(name))
+
+def calc(request, a, b):
+    return HttpResponse("{}+{}={}".format(a, b, a+b))
+
+def getuser(request):
+    name = request.GET['name']
+    return HttpResponse("Name: {}".format(name))
+
+
+
+def cookie(request):
+    response = HttpResponse(content_type='text/html')
+    response.set_cookie('author', 'Lalak')
+
+    if 'page_views' in request.session:
+        request.session['page_views'] += 1
+    else:
+        request.session['page_views'] = 1
+
+    cookies = request.COOKIES
+
+    html = '''
+    <html>
+    <body>
+        <h2>Cookies:</h2>
+        <ul>
+    '''
+
+    if cookies:
+        for key, value in cookies.items():
+            html += f"<li><strong>{key}:</strong> {value}</li>"
+    else:
+        html += "<li>No cookies available.</li>"
+
+    html += f'''
+        </ul>
+        <h2>Page Views: {request.session['page_views']}</h2>
+    </body>
+    </html>
+    '''
+
+    response.content = html
+    return response
+
+
