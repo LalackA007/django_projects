91f32090a5dfa61eed28b7f6ec50d1b6162fe02b
ТехноРай <sofialalacha@007>
2024-10-28 23:17:52 +0200

    registration

--- gameshop/products/urls.py
+++ gameshop/products/urls.py
@@ -1,3 +1,4 @@
+# urls.py
 from django.urls import path
 from . import views
 
@@ -5,11 +6,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accessories/', views.accessories, name='accessories'),
     path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name='checkout'),
     path('add_product_to_cart/<int:product_id>/', views.add_product_to_cart, name='add_product_to_cart'),
     path('add_accessory_to_cart/<int:accessory_id>/', views.add_accessory_to_cart, name='add_accessory_to_cart'),
     path('update_cart/<int:item_id>/', views.update_cart, name='update_cart'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup_view, name='signup'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
