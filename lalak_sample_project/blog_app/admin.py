91f1c14f183115ba2ce4049eb7b745bae74ab94e
ТехноРай <sofialalacha@007>
2024-11-20 21:54:21 +0200

    auto-slug

--- lalak_sample_project/blog_app/admin.py
+++ lalak_sample_project/blog_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import BlogPost
 
-admin.site.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(BlogPost, BlogPostAdmin)
