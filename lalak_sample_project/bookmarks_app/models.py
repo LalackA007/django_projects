a96bce4670ce2b94f65f1881ee9e72cf732a784c
ТехноРай <sofialalacha@007>
2024-12-10 13:04:10 +0200

    successful auth

--- lalak_sample_project/bookmarks_app/models.py
+++ lalak_sample_project/bookmarks_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Image(models.Model):
+    url = models.URLField(max_length=2000)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='image_created', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created_at']),
+        ]
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- lalak_sample_project/lalak_sample_project/settings.py
+++ lalak_sample_project/lalak_sample_project/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'crispy_forms',  
     'crispy_bootstrap5',  
     'django_extensions',
-    'social_django', 
+    'social_django',
+    'bookmarks_app',
 ]
 
 MIDDLEWARE = [
@@ -159,7 +160,7 @@ AUTHENTICATION_BACKENDS = [
     'home_app.authentication.EmailAuthBackend',
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.github.GithubOAuth2',
-    'account.authentication.EmailAuthBackend',
+    'home_app.authentication.EmailAuthBackend',
 ]
 
 SOCIAL_AUTH_PIPELINE = [
@@ -169,7 +170,7 @@ SOCIAL_AUTH_PIPELINE = [
  'social_core.pipeline.social_auth.social_user',
  'social_core.pipeline.user.get_username',
  'social_core.pipeline.user.create_user',
- 'account.authentication.create_profile',
+#  'home_app.authentication.create_profile',
  'social_core.pipeline.social_auth.associate_user',
  'social_core.pipeline.social_auth.load_extra_data',
  'social_core.pipeline.user.user_details',
