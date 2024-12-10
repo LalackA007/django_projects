e9bf8a633d6e300f949213dd8217f53dc252fce4
ТехноРай <sofialalacha@007>
2024-12-10 08:25:31 +0200

    -auth

--- lalak_sample_project/lalak_sample_project/settings.py
+++ lalak_sample_project/lalak_sample_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-42kb)k9x$s0j0d!i%a-izs8fhczcf()nj)bbv+d&tds$e(kf$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions', 
     'crispy_forms',  
     'crispy_bootstrap5',  
+    'django_extensions',
+    'social_django', 
 ]
 
 MIDDLEWARE = [
@@ -147,7 +148,29 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  BASE_DIR / 'media'
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
+
+SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET')
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'home_app.authentication.EmailAuthBackend'
+    'home_app.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'account.authentication.EmailAuthBackend',
+]
+
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
 ]
